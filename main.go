5cd714970f6e49e0d382f9e6458b99b799f84cec
Sef Tarbell <sef.tarbell@pm.me>
2020-09-07 07:21:20 -0500

    Added unmarshall example

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type person struct {
+	First string
+}
+
+func main() {
+	p1 := person{
+		First: "Frida",
+	}
+
+	p2 := person{
+		First: "Marcus",
+	}
+
+	xp := []person{p1, p2}
+
+	bs, err := json.Marshal(xp)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(string(bs))
+
+	xp2 := []person{}
+
+	err = json.Unmarshal(bs, &xp2)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println("back into GO data", xp2)
+}
