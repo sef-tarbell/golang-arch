8b2a5bd9ab9bce0338b212c31fa14b92642244bd
Sef Tarbell <sef.tarbell@pm.me>
2020-09-15 12:18:00 -0500

    First implementation of amazon oauth login

--- go.mod
+++ go.mod
@@ -4,5 +4,8 @@ go 1.15
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gofrs/uuid v3.3.0+incompatible
+	github.com/google/uuid v1.1.2
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43
 )
--- main.go
+++ main.go
@@ -1,101 +1,309 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
-	"strings"
+	"net/url"
+	"time"
 
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/github"
+	"golang.org/x/oauth2/amazon"
 )
 
-// {"data":{"viewer":{"id":"<returned-from-github>"}}}
-type githubOAuthResponse struct {
+// {"data":{"viewer":{"id":"<returned-from-amazon>"}}}
+/*
+type amazonOAuthResponse struct {
 	Data struct {
 		Viewer struct {
 			ID string `json:"id"`
 		} `json:"viewer"`
 	} `json:"data"`
 }
+*/
 
-var githubOauthConfig = &oauth2.Config{
-	ClientID:     "e21fc64e6333e75e190c",
-	ClientSecret: "8d6a0ce82f6d081e6c998d1bb9fc5556592a30e4",
-	Endpoint:     github.Endpoint,
+type UserData struct {
+	UserName     string
+	FirstName    string
+	Registration string
+	LastLogin    string
+	Password     []byte
 }
 
-// key is github ID, value is user ID
-var githubConnections map[string]string
+// key is username, value is userdata
+var db = map[string]UserData{}
+
+// key is sessionid, value is token
+var sessions = map[string]string{}
+
+// key is uuid from oauth login, value is expiration time
+var oauthExp = map[string]time.Time{}
+
+var amazonOAuthConfig = &oauth2.Config{
+	ClientID:     "amzn1.application-oa2-client.441cca64a275479194c4ae19bf32b33b",
+	ClientSecret: "106ff65dbe038691be6c03211ff64cd02d14d8215149e195f36949a06ec87771",
+	Endpoint:     amazon.Endpoint,
+	RedirectURL:  "http://localhost:8080/oauth/amazon/receive",
+	Scopes:       []string{"profile"},
+}
+
+// key is amazon ID, value is user ID
+var amazonConnections map[string]string
 
 func main() {
 	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/oauth/github", oauthGithubHandler)
-	http.HandleFunc("/oauth2/receive", oauthReceiveHandler)
+	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/oauth/amazon/login", oauthAmazonLogin)
+	http.HandleFunc("/oauth/amazon/receive", oauthAmazonReceive)
 	http.ListenAndServe(":8080", nil)
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("session")
+	if err != nil {
+		c = &http.Cookie{
+			Name:  "session",
+			Value: "",
+		}
+	}
+
+	sid, err := parseToken(c.Value)
+	if err != nil {
+		log.Println("root failed to parseToken", err)
+	}
+
+	var userName string
+	if sid != "" {
+		userName = sessions[sid]
+	}
+
+	var firstName string
+	if userData, ok := db[userName]; ok {
+		firstName = userData.FirstName
+	}
+
+	msg := r.FormValue("msg")
+
 	fmt.Fprintf(w,
 		`<!DOCTYPE html>
 		<html lang="en">
 		<head>
 			<meta charset="UTF-8">
-			<title>Title</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>OAuth Example</title>
 		</head>
 		<body>
-			<form action="/oauth/github" method="POST">
-				<input type="submit" value="Login with Github" />
+			<p>Logged in as <b>%s</b> (%s)</p>
+			<p>Message: %s</p>
+			<hr>
+			<h3>Register</h3>
+			<form action="/register" method="POST">
+				<label for="firstname">First Name:</label>
+					<input type="text" name="firstname" placeholder="First Name" id="firstname" /><br />
+				<label for="username">User Name:</label>
+					<input type="text" name="username" placeholder="User Name" id="username" /><br />
+				<label for="password">Password:</label>
+					<input type="password" name="password" id="password" /><br />
+				<button type="submit">Register</button>
+			</form>
+			<hr>
+			<h3>Login</h3>
+			<form action="/login" method="POST">
+				<label for="username">User Name:</label>
+					<input type="text" name="username" placeholder="User Name" id="username" /><br />
+				<label for="password">Password:</label>
+					<input type="password" name="password" id="password" /><br />
+				<button type="submit">Login</button>
+			</form>
+			<hr>
+			<h3>Login With Amazon</h3>
+			<form action="/oauth/amazon/login" method="POST">
+				<button type="submit">Login With Amazon</button>
+			</form>
+			<hr>
+			<h3>Logout</h3>
+			<form action="/logout" method="POST">
+				<button type="submit">Logout</button>
 			</form>
 		</body>
-		</html>`)
+		</html>`, firstName, userName, msg)
 }
 
-func oauthGithubHandler(w http.ResponseWriter, r *http.Request) {
-	// should be passing some sort of state - session or something
-	redirectURL := githubOauthConfig.AuthCodeURL("0000")
-	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+func register(w http.ResponseWriter, r *http.Request) {
+	// safety check, only handle POST requests
+	if r.Method != http.MethodPost {
+		errorMsg := url.QueryEscape("HTTP method was not a POST")
+		http.Redirect(w, r, "/?msg="+errorMsg, http.StatusSeeOther)
+		return
+	}
+
+	// get the form data
+	userName := r.FormValue("username")
+	firstName := r.FormValue("firstname")
+	password := r.FormValue("password")
+
+	// safety check username and password required
+	if userName == "" || password == "" {
+		errorMsg := url.QueryEscape("Missing required data")
+		http.Redirect(w, r, "/?msg="+errorMsg, http.StatusSeeOther)
+		return
+	}
+
+	// create a hash of the password
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		errorMsg := "Internal Server Error"
+		http.Error(w, errorMsg, http.StatusInternalServerError)
+		return
+	}
+	password = ""
+
+	u := UserData{
+		UserName:     userName,
+		FirstName:    firstName,
+		Registration: time.Now().Format(time.RFC3339),
+		LastLogin:    time.Now().Format(time.RFC3339),
+		Password:     hash,
+	}
+	db[userName] = u
+
+	// create a uuid for session id
+	sid := uuid.New().String()
+	sessions[sid] = userName
+
+	// create a token to store as cookie
+	t, err := createToken(sid)
+	if err != nil {
+		errorMsg := "Internal Server Error"
+		http.Error(w, errorMsg, http.StatusInternalServerError)
+		return
+	}
+
+	// store cookie
+	c := http.Cookie{
+		Name:  "session",
+		Value: t,
+		Path:  "/",
+	}
+
+	// return with no error
+	http.SetCookie(w, &c)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
-func oauthReceiveHandler(w http.ResponseWriter, r *http.Request) {
-	code := r.FormValue("code")
-	state := r.FormValue("state")
-
-	// this would check the session id
-	if state != "0000" {
-		http.Error(w, "State is incorrect", http.StatusBadRequest)
+func login(w http.ResponseWriter, r *http.Request) {
+	// safety check, only handle POST requests
+	if r.Method != http.MethodPost {
+		errorMsg := url.QueryEscape("HTTP method was not a POST")
+		http.Redirect(w, r, "/?msg="+errorMsg, http.StatusSeeOther)
 		return
 	}
 
-	token, err := githubOauthConfig.Exchange(r.Context(), code)
-	if err != nil {
-		http.Error(w, "Login failed", http.StatusUnauthorized)
+	// get the form data
+	userName := r.FormValue("username")
+	password := r.FormValue("password")
+
+	// safety check username and password required
+	if userName == "" || password == "" {
+		errorMsg := url.QueryEscape("Missing required data")
+		http.Redirect(w, r, "/?msg="+errorMsg, http.StatusSeeOther)
 		return
 	}
 
-	ts := githubOauthConfig.TokenSource(r.Context(), token)
-	client := oauth2.NewClient(r.Context(), ts)
-
-	requestBody := strings.NewReader(`{"query": "query {viewer {id}}"}`)
-	resp, err := client.Post("https://api.github.com/graphql", "application/json", requestBody)
-	if err != nil {
-		http.Error(w, "Failed to retrieve user id", http.StatusBadRequest)
-		return
-	}
-	defer resp.Body.Close()
-
-	var gr githubOAuthResponse
-	err = json.NewDecoder(resp.Body).Decode(&gr)
-	if err != nil {
-		http.Error(w, "Github invalid response", http.StatusInternalServerError)
-		return
-	}
-
-	githubID := gr.Data.Viewer.ID
-	userID, ok := githubConnections[githubID]
+	// user is in the db
+	u, ok := db[userName]
 	if !ok {
-		// create user account
+		errorMsg := url.QueryEscape("Username or password mismatch")
+		http.Redirect(w, r, "/?msg="+errorMsg, http.StatusSeeOther)
+		return
 	}
 
-	// login to account using JWT
+	// password check
+	err := bcrypt.CompareHashAndPassword(u.Password, []byte(password))
+	if err != nil {
+		errorMsg := url.QueryEscape("Username or password mismatch")
+		http.Redirect(w, r, "/?msg="+errorMsg, http.StatusSeeOther)
+		return
+	}
+	password = ""
+
+	// create a uuid for session id
+	sid := uuid.New().String()
+	sessions[sid] = userName
+
+	u.LastLogin = time.Now().Format(time.RFC3339)
+	db[userName] = u
+
+	// create a token to store as cookie
+	t, err := createToken(sid)
+	if err != nil {
+		errorMsg := "Internal Server Error"
+		http.Error(w, errorMsg, http.StatusInternalServerError)
+		return
+	}
+
+	// store cookie
+	c := http.Cookie{
+		Name:  "session",
+		Value: t,
+		Path:  "/",
+	}
+
+	// return with no error
+	http.SetCookie(w, &c)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	sid := ""
+	message := "Logged out"
+
+	// retrieve the session id from the cookie
+	if c, err := r.Cookie("session"); err == nil {
+		// parse the cookie to get the session id
+		sid, err = parseToken(c.Value)
+		if err != nil {
+			// log out an error?
+			message = "Failed to parse cookie"
+		}
+
+		if sid != "" {
+			// delete the session
+			delete(sessions, sid)
+		}
+
+		newCookie := http.Cookie{
+			Name:    "session",
+			Value:   "",
+			Path:    "/",
+			Expires: time.Unix(0, 0),
+		}
+
+		http.SetCookie(w, &newCookie)
+	}
+
+	http.Redirect(w, r, "/?msg="+message, http.StatusSeeOther)
+}
+
+func oauthAmazonLogin(w http.ResponseWriter, r *http.Request) {
+	// safety check, only handle POST requests
+	if r.Method != http.MethodPost {
+		errorMsg := url.QueryEscape("HTTP method was not a POST")
+		http.Redirect(w, r, "/?msg="+errorMsg, http.StatusSeeOther)
+		return
+	}
+
+	// create a uuid for session id
+	id := uuid.New().String()
+	oauthExp[id] = time.Now().Add(time.Hour)
+
+	// redirect to amazon for login
+	http.Redirect(w, r, amazonOAuthConfig.AuthCodeURL(id), http.StatusSeeOther)
+}
+
+func oauthAmazonReceive(w http.ResponseWriter, r *http.Request) {
 }
